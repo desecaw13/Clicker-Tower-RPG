adb45d1799b3b668314676aff99c2d268de0659b
desecaw13 <47424966+desecaw13@users.noreply.github.com>
2020-07-02 11:51:16 -0700

    started enemy script
    this should work

---
+++ Assets/EnemyControl.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyControl : MonoBehaviour
+{
+    public int speed;
+
+    private void Start()
+    {
+        speed = 1;
+    }
+}
--- Assets/PlayerS.cs
+++ Assets/PlayerS.cs
@@ -5,20 +5,28 @@ using UnityEngine;
 public class PlayerS : MonoBehaviour
 {
     //player stats
-    public int Level = 1;
-    public int Gold = 0;
-    public int Exp = 0;
+    public int Level;
+    public int Gold;
+    public int Exp;
 
-    public int currHP = 10;
-    public int maxHP = 10;
-    public int Str = 1;
-    public int Def = 1;
-    public int Speed = 1;
-    public int Luck = 1;
+    public int currHP;
+    public int maxHP;
+    public int Str;
+    public int Def;
+    public int Speed;
+    public int Luck;
 
     void Start()
     {
-        //;
+        Level = 1;
+        Gold = 0;
+        Exp = 0;
+        currHP = 10;
+        maxHP = 10;
+        Str = 1;
+        Def = 1;
+        Speed = 1;
+        Luck = 1;
     }
 
     void Update()
