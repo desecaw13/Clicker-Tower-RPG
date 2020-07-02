f5c7ba637937b49871b65eb797926c142e04aacd
desecaw13 <47424966+desecaw13@users.noreply.github.com>
2020-07-01 21:28:12 -0700

    started creating UI
    added the starts of a ui system, and wrote example code for buttons

---
+++ Assets/MenuControl.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuControl : MonoBehaviour
+{
+    public void ButtonEX ()
+    {
+        //changing a scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        Debug.Log("Exmaple text");
+    }
+
+    public void actionButton()
+    { }
+}
